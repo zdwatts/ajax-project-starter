c9a09e27fd02fd5d7b9419c9ae1acdf30a6bbfe8
zacwatts <zdwatts@gmail.com>
2020-10-06 13:33:13 -0600

    finished phase 4

--- public/events.js
+++ public/events.js
@@ -1,11 +1,72 @@
 window.addEventListener('DOMContentLoaded',()=>{
 
-fetch('/kitten/image')
-    .then(res =>{
-        if(!res.ok){
-            throw res
-        }
-        return res.json();
+    fetch('/kitten/image')
+        .then(res => {
+            //console.log("res:", res);
+            if(!res.ok){
+                throw res
+            }
+            return res.json();
+        }).then(data => {
+            console.log("data:", data);
+            let pic = document.querySelector(".cat-pic")
+            pic.src = data.src;
+        }).catch(err => {
+            err.json().then(data => {
+                document.querySelector(".error").innerHTML = data.message;
+            })
+            alert("Something went wrong! Please try again!")
+        })
+
+    document.getElementById("new-pic").addEventListener("click", () => {
+        document.querySelector(".loader").innerHTML = "loading..."
+        document.querySelector(".error").innerHTML = ""
+        fetch("/kitten/image")
+            .then(res => {
+                // console.log("res:", res);
+                if(!res.ok){
+                    throw res
+                }
+                return res.json();
+            }).then(data => {
+                console.log("data:", data);
+                let pic = document.querySelector(".cat-pic")
+                pic.src = data.src;
+            }).catch(err => {
+                err.json().then(data => {
+                    document.querySelector(".error").innerHTML = data.message;
+                })
+                alert("Something went wrong! Please try again!")
+            })
     })
 
+    document.getElementById("upvote").addEventListener("click", () => {
+        fetch("/kitten/upvote", { method: "PATCH" })
+        .then(res => {
+            //console.log("res:", res)
+            if(!res.ok){
+                throw res
+            }
+            return res.json();
+        }).then(data => {
+            document.querySelector(".score").innerHTML = data.score;
+        }).catch(err => {
+            document.querySelector(".error").innerHTML = "Something went wrong"
+        })
+    })
+
+    document.getElementById("downvote").addEventListener("click", () => {
+        fetch("/kitten/downvote", { method: "PATCH" })
+        .then(res => {
+            //console.log("res:", res)
+            if(!res.ok){
+                throw res
+            }
+            return res.json();
+        }).then(data => {
+            document.querySelector(".score").innerHTML = data.score;
+        }).catch(err => {
+            document.querySelector(".error").innerHTML = "Something went wrong"
+        })
+    })
 })
